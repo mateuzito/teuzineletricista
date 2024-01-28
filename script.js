4ae9d9fb5b2364d1e87fb6184a83b7c723d5bae3
MateusZitoSilva <mateuszitosilva@gmail.com>
2024-01-28 13:33:10 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+
+document.querySelector('.sidebar').classList.toggle('open');    
+}
+
+function closeNav() {
+
+document.querySelector('.sidebar').classList.remove('open');    
+}
